e8e78af817878de44740d8786af6f1b13114cb04
yanachuwan9sm <gurem919@icloud.com>
2022-08-07 00:01:53 +0900

    - add : 🚀 styled-componentのcompilerオプションの有効化・SSRに対応するためにdocument.tsxの作成

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  compiler: {
+    // styled-componentsの有効化
+    styledComponents: true,
+  },
+};
+module.exports = nextConfig;
